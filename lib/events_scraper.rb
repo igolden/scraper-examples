a2354dab5ea52c807486d1201bacab88922dd0bb
Ian Golden <ian@iangolden.com>
2018-01-07 15:11:29 -0700

    Rails runner downloading the page correctly

--- lib/events_scraper.rb
+++ lib/events_scraper.rb
@@ -1,12 +1,16 @@
-class EventScraper
+class EventsScraper
   def intialize
     raise StandardError, "You can't instantiate this class"
   end
 
   # class methods
   class << self
-    def download_events
-      `wget https://www.builtincolorado.com/events`
+    def download_events(pagenum=1)
+      save_path = File.join(Rails.root, "lib/data")
+
+      `cd #{save_path} && wget https://www.builtincolorado.com/events?page=#{pagenum} -O #{pagenum}`
     end
   end
 end
+
+EventsScraper.download_events
